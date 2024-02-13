6d7647d486fe26c60e34df3a9929e2950fa325c7
FeliOjedaRuiz <feliojedaruiz@gmail.com>
2024-02-13 13:04:43 +0100

    In web proyect add react-router-dom and HomePage route

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,25 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>Jurado App</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
