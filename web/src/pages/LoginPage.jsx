45231da90d78a75e94318a3efcc8df76ae6920d1
FeliOjedaRuiz <feliojedaruiz@gmail.com>
2024-02-17 01:48:33 +0100

    Login Layout done

--- web/src/pages/LoginPage.jsx
+++ web/src/pages/LoginPage.jsx
@@ -1,16 +1,38 @@
+import { useEffect, useState } from "react";
 import LayoutLogin from "../components/layouts/LayoutLogin";
+import LoginImage from "../images/cerrar-mujer-sosteniendo-smartphone.jpg";
 
 function LoginPage() {
+  const [mobile, setMobile] = useState(true)
+  const [changeSize, setChageSize] = useState(true);
+
+  useEffect(() => {
+    if (window.innerWidth < "960") {
+      console.log("menor")
+      setMobile(true);
+    } else {
+      console.log("mayor")
+      setMobile(false);
+    }
+  }, [changeSize]);
+  window.onresize = function (event) {
+    setChageSize(!changeSize);
+    console.log("resize");
+  };
+
   return (
     <LayoutLogin>
-      <div className="flex  h-[calc(100vh-58px)] w-full">
-        <div className="w-1/4 bg-gray-100">Formulario</div>
-        <div className="flex flex-col bg-gray-200 justify-center w-full">
-          <h1 className="">Datos </h1>
-          <h1 className="">Datos </h1>
-          <h1 className="">Datos </h1>
-          <h1 className="">Datos </h1>
+      <div className="flex h-[calc(100vh-58px)]">
+        <div className="w-full lg:w-1/4 bg-gray-100">
+          <h6>Formulario</h6>
         </div>
+        {!mobile && (<div className="w-3/4">
+          <img
+            className="object-cover w-full h-full"
+            src={LoginImage}
+            alt="Mujer sosteniendo móvil"
+          />
+        </div>)}
       </div>
     </LayoutLogin>
   );
